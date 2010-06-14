7c8efb5443b3dee7639eaa187b2c1bea1a8465ee
Dario Sestito <darios@symbian.org>
2010-06-14 16:03:06 +0100

    Add wrapper .bat file for build_package.pl

---
+++ build_package.bat
@@ -0,0 +1,15 @@
+@REM Copyright (c) 2009 Symbian Foundation Ltd
+@REM This component and the accompanying materials are made available
+@REM under the terms of the License "Eclipse Public License v1.0"
+@REM which accompanies this distribution, and is available
+@REM at the URL "http://www.eclipse.org/legal/epl-v10.html".
+@REM
+@REM Initial Contributors:
+@REM Symbian Foundation Ltd - initial contribution.
+@REM
+@REM Contributors:
+@REM
+@REM Description:
+@REM Invokes the tool by call to the related interpreter
+
+@perl %~dp0\build_package.pl %*
